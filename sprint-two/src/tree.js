3baaa718538252e7874cd6b05c3e14a11a6119f5
luisgnava <luis.gnava85@gmail.com>
2018-01-16 09:06:31 -0600

    Finished Tree, added final pieces of code

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,29 @@
-var Tree = function(value) {
-  var newTree = {};
-  newTree.value = value;
+var newTree = {};
+newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
-  return newTree;
+Object.assign(newTree, treeMethods);
+newTree.children = [];
+return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
-
 
 
 /*
